8edcb0337e97d8aa962deb6dfb97f5f07040de89
ToineF <antoine.foucault777@gmail.com>
2023-06-27 15:57:22 +0200

    button css issue

--- src/componants/layoutbutton.js
+++ src/componants/layoutbutton.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
 
 export default function LayoutButton({ link, text, color }) {
+  const buttonclass = "rounded-md border px-4 py-2 text-gray-50 ";
   return (
     <Link href={link}>
-      <button className={`rounded-md border px-4 py-2 ${color} text-gray-50`}>
+      <button className={`${buttonclass} ${color}`}>
         {text}&nbsp;&nbsp;&nbsp;&gt;
       </button>
     </Link>
