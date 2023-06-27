3f34977bfeee3b7b6ca6aeb409586312d4a668d9
ToineF <antoine.foucault777@gmail.com>
2023-06-27 17:29:23 +0200

    id page country

--- src/pages/countries/[id].js
+++ src/pages/countries/[id].js
@@ -1,7 +1,31 @@
-export default function CountryID() {
+export const getStaticPaths = async () => {
+  const res = await fetch("https://restcountries.com/v3.1/all");
+  const data = await res.json();
+  const paths = data.map((c) => {
+    return {
+      params: { id: c.name.common.toString() },
+    };
+  });
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch(`https://restcountries.com/v3.1/all/${id}`);
+  const data = await res.json();
+  console.log(data);
+  return {
+    props: {
+      country: data,
+    },
+  };
+};
+
+export default function CountryID({ country }) {
   return (
     <div className="flex min-h-screen flex-col items-center justify-start gap-8 p-24">
-      <p>Hello</p>
+      {console.log(country)}
+      <p>COUNTRY PAGE</p>
     </div>
   );
 }
